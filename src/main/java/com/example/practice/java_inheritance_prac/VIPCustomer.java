2ac5af4e486b714683e798f9557bb01836eecdef
roy656 <v0o0605@gmail.com>
2022-10-26 23:46:07 +0900

    상속 Override 와 super 쓰임새

--- src/main/java/com/example/practice/java_inheritance_prac/VIPCustomer.java
+++ src/main/java/com/example/practice/java_inheritance_prac/VIPCustomer.java
@@ -11,7 +11,7 @@ public class VIPCustomer extends Customer {
         super(customerId, customerName);
                                         // 하위 클래스의 생성자가 호출될때 상위 클래스의 생성자가 먼저 호출된다.
         customerLevel = "VIP";          // 상위클래스를 호출하는 코드가 없으면 컴파일러가 자동으로 super(); 를 자동으로 넣어준다
-        bonusRatio = 0.05;              // super(); 로 상위 클래스의 참조값을 가지게 한다.
+        bonusRatio = 0.05;              // 만약 매개변수가 있는 생성자 라면 super(); 로 명시적으로 호출하여 상위 클래스의 참조값을 가지게 한다.
         discountRatio = 0.01;
     }
 
