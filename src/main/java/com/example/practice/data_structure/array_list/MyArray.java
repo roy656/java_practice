090998cedce6246429a83bd3c86af578f3fd1512
Roy C <v0o0605@gmail.com>
2022-11-07 21:13:30 +0900

    자료구조 중 배열

--- src/main/java/com/example/practice/data_structure/array_list/MyArray.java
+++ src/main/java/com/example/practice/data_structure/array_list/MyArray.java
@@ -32,7 +32,7 @@ public class MyArray {
     }
 
     // 배열 중간에 데이터 삽입 (그냥 넣으면 오버라이팅 되기 때문에 넣을 위치 뒤의 데이터들을 밀고 넣는다)
-    public void inserElement(int position, int num) {       // 매개변수는 넣을 위치, 데이터 값
+    public void insertElement(int position, int num) {       // 매개변수는 넣을 위치, 데이터 값
         int i;
 
         if(position<0 || position>count) {          // index 가 허용 범위 밖일때 에러
@@ -48,7 +48,7 @@ public class MyArray {
         }
 
         intArr[position] = num;
-        count++;                                    // 메소드 실행때 마다 요소가 하나씩 늘어난다
+        count++;                                    // 메소드 실행때 마다 요소가 하나씩 늘어난거
 
     }
 
@@ -75,5 +75,41 @@ public class MyArray {
 
     }
 
+    public int getSize() {
+        return count;
+    }
 
-}
+    public boolean isEmpty() {
+        if( count == 0) {
+            return true;
+        } else {
+            return false;
+        }
+    }
+
+    public int getElement(int position) {
+        if( position < 0 || position > count-1) {
+            System.out.println("인덱스가 허용 범위를 벗어났습니다.");
+            return ERROR_NUM;
+        }
+        return intArr[position];
+    }
+
+    public void printll() {
+        if (count == 0) {
+            System.out.println("출력할 내용이 없습니다.");
+        }
+
+        for (int i = 0; i < count; i++) {
+            System.out.println(intArr[i]);
+        }
+    }
+
+        public void removeAll() {
+
+            for(int i=0; i<count; i++){
+                intArr[i] = 0;
+            }
+            count = 0;
+        }
+    }
