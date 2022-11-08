fcb9f232d7cee77f1287c9f8096c0939b372a1ca
Roy C <v0o0605@gmail.com>
2022-11-08 23:31:54 +0900

    Generic 타입

--- src/main/java/com/example/practice/java_generic_prac/ThreeDPrinter1.java
+++
@@ -1,8 +0,0 @@
-package com.example.practice.java_generic_prac;
-
-public class ThreeDPrinter1 {
-
-    private Powder material;
-
-
-}
--- src/main/java/com/example/practice/java_generic_prac/ThreeDPrinter2.java
+++
@@ -1,9 +0,0 @@
-package com.example.practice.java_generic_prac;
-
-public class ThreeDPrinter2 {
-
-    private Plastic material;
-
-
-    
-}
