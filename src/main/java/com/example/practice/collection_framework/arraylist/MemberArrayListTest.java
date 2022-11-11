01f18ebb5511a9d6f1d794877a68d06784327e61
Roy C <v0o0605@gmail.com>
2022-11-11 23:03:36 +0900

    Collection 프레임워크 중 List 인터페이스를 이용한 Member 관리

---
+++ src/main/java/com/example/practice/collection_framework/arraylist/MemberArrayListTest.java
@@ -0,0 +1,25 @@
+package com.example.practice.collection_framework.arraylist;
+
+public class MemberArrayListTest {
+
+    public static void main(String[] args) {
+
+        MemberArrayList memberArrayList = new MemberArrayList();
+
+        Member memberCol = new Member(10001, "Colin");
+        Member memberRoy = new Member(10002, "Roy");
+        Member memberKwan = new Member(10003, "Kwan");
+        Member memberMartin = new Member(10004, "Martin");
+
+        memberArrayList.addMember(memberCol);
+        memberArrayList.addMember(memberRoy);
+        memberArrayList.addMember(memberKwan);
+        memberArrayList.addMember(memberMartin);
+
+        memberArrayList.showAllMember();
+
+        memberArrayList.removeMember(memberMartin.getMemberId());
+
+        memberArrayList.showAllMember();
+    }
+}
