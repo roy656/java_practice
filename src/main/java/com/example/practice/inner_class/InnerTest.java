a1e1a92b55d36ce4173a7e55d92580e0817124d9
Roy C <v0o0605@gmail.com>
2022-11-13 22:51:46 +0900

    인스턴스 내부 클래스 연습

---
+++ src/main/java/com/example/practice/inner_class/InnerTest.java
@@ -0,0 +1,39 @@
+package com.example.practice.inner_class;
+
+class OutClass {
+
+    private int num = 10;
+    private static  int sNum = 20;
+    private InClass inClass;
+
+    public OutClass() {             // Inner Class 는 Out Class 가 먼저 생성 되고 난 후에 생성 가능.
+        inClass = new InClass();
+    }
+
+
+    private class InClass {             // 그렇기 때문에 Inner Class 에서는 static 변수를 사용할 수 없다 ( static 변수는 객체의 생성과 상관 없이 쓸수 있으므로)
+
+        int iNum = 100;
+
+        void inTest() {
+            System.out.println("OutClass num = " + num + "(외부 클래스의 인스턴스 변수)");
+            System.out.println("OutClass sNum = " + num + "(외부 클래스의 static 변수)");
+            System.out.println("InClass iNum = " + iNum + "(내부 클래스의 인스턴스 변수)");
+
+        }
+    }
+
+    public void usingClass() {
+        inClass.inTest();
+    }
+}
+
+public class InnerTest {
+
+    public static void main(String[] args) {
+
+        OutClass outClass = new OutClass();
+        outClass.usingClass();
+
+    }
+}
