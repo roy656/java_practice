ec9560c335805816efa538868bd69cf110930d96
Roy C <v0o0605@gmail.com>
2022-11-19 22:13:06 +0900

    람다식 기본 사용 연습

--- src/main/java/com/example/practice/java_lamda_prac/Add.java
+++ src/main/java/com/example/practice/java_lamda_prac/Add.java
@@ -2,4 +2,5 @@ package com.example.practice.java_lamda_prac;
 
 public interface Add {
     public int add(int x, int y);
+
 }
--- src/main/java/com/example/practice/java_lamda_prac/AddTest.java
+++ src/main/java/com/example/practice/java_lamda_prac/AddTest.java
@@ -9,5 +9,10 @@ public class AddTest {
 //        Add addC = (x, y)-> x+y;              // 람다식의 생략 규칙에 따라 생략가능한 부분 존재.
 
         System.out.println(addC.add(2,3));
+
+        Add mul = (x,y) -> {return x*y;};
+
+        System.out.println(mul.add(4,5));
+
     }
 }
